47e3aedada4127c89346132b7ab1bed4437c6de9
ElzarMachado <101478824+ElzarMachado@users.noreply.github.com>
2022-04-23 23:37:55 -0700

    Add project files.

---
+++ ListasSimplementeLigadas/Nodo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListasSimplementeLigadas
+{
+    public class Nodo
+    {
+        public string Valor { get; set; }
+        public Nodo Enlace { get; set; }
+
+        public Nodo(string valor = "", Nodo enlace = null)
+        {
+            Valor = valor;
+            Enlace = enlace;
+        }
+    }
+}
+
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
